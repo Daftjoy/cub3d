913a69cc82bd9331d142a38879d919c51425ab51
Antonio Martin espinosa <antmarti@c1r6s3.42madrid.com>
2020-02-10 17:16:29 +0100

    2

--- cub3d.c
+++ cub3d.c
@@ -1,4 +1,18 @@
 #include "mlx.h"
+#include <unistd.h>
+
+void 	ft_putchar (char c)
+{
+	write (1, &c, 1);
+}
+
+int deal_key(int key, void *param)
+{
+	if (key == 67)
+		ft_putchar('c');
+	ft_putchar('x');
+	return (0);
+}
 
 int main()
 {
@@ -7,9 +21,9 @@ int main()
 	void *lls_ptr;
 	
 	mlx_ptr = mlx_init(); /*necesario para iniciar la conexión con el paqute */
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "PAJúo" ); /*win_ptr  hace que las funciones del loop se asocien especçificamente con esta ventana*/
-	lls_ptr = _pixel_put ( mlx_ptr, win_ptr, 20, 20, 56 );
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "PAJuo" ); /*win_ptr  hace que las funciones del loop se asocien especificamente con esta ventana*/
+	mlx_pixel_put (mlx_ptr, win_ptr, 200, 200, 0xFFFFFF );
+	mlx_key_hook(win_ptr, deal_key, (void *)0);
 	mlx_loop(mlx_ptr); /* hace que se quede esperando a que hagas algo*/
-	mlx_loop(lls_ptr); /* hace que se quede esperando a que hagas algo*/
 	return (0);
 }
